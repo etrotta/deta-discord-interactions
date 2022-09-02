988da800fdc5be1b7f28888acd835f0aa50c3093
Eric Trotta <erictrottavolt@gmail.com>
2022-09-02 15:45:34 -0300

    Add Permissions enum

--- deta_discord_interactions/enums/__init__.py
+++ deta_discord_interactions/enums/__init__.py
@@ -1,5 +1,7 @@
 from deta_discord_interactions.enums.response_types import ResponseType
+from deta_discord_interactions.enums.permissions import Permission
 
 __all__ = [
     "ResponseType",
+    "Permission",
 ]
